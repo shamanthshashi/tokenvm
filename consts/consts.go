c1712f0ddab4fa586a725d20ec14520a7a8314ea
shamanthshashi <shamanthshashi2@gmail.com>
2024-06-01 09:55:21 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Shamanth"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Shamanth TS"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "STS"
 )
 
 var ID ids.ID
